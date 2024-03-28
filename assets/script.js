528c1056572a9286ec3eef91d0de16c8586ea9c0
Stephanie Carr <sacarr91@gmail.com>
2024-03-28 15:40:07 -0400

    huge modification - refactored code, finished assignment

--- assets/script.js
+++ assets/script.js
@@ -1,57 +1,98 @@
 // Get a reference to the #add-employees-btn element
 const addEmployeesBtn = document.querySelector('#add-employees-btn');
 
-// !!!!!! 
-// Major thanks to Clarissa Mobley & Zach Polof for their help in refactoring my code! (Variables & while statement)
-// !!!!!!
+// Major thanks to Clarissa Mobley & Zach Polof for their help in refactoring my code (lines 57-65 & 74)!
 
-const collectEmployees = function() { //begin process of collecting group of employee data
- 
-  let employeesArray = [] //intitiate empty array in which to collect employee data
-  let nextEmployee = true //provide condition needed to run while loop
+let employeesArray = []; //intitiate empty array in which to collect employee data
 
-  //establish variables to be levied in the function
+//function to collect employee data from user input
+const collectEmployees = function() { 
+
+  //provide condition needed to run while loop
+  let nextEmployee = true; 
+
+  //WHILE LOOP to collect employee data
+  while (nextEmployee) { 
   
-  while (nextEmployee) { //collect employee data
-    const firstName = prompt('First Name plz')
-    const lastName = prompt('last name')
-    let salary = prompt('salary')
+    //EMPLOYEE INFO INPUT CODE BLOCK
+    const firstName = prompt('Employee First Name');
+      if (firstName === null) {
+        nextEmployee = false;
+        console.log(`Operation cancelled at First Name entry`);
+        break;
+      };
+      if (firstName === undefined) {
+        alert(`Please enter a name in the box provided.`);
+        firstName = prompt('Employee First Name');
+      };
+    console.log(`First name entered: ${firstName}`);
+    
+    const lastName = prompt('Employee Last Name');
+      if (lastName === null) {
+        console.log(`Operation cancelled at Last Name entry`);
+        nextEmployee = false;
+        break;
+      };
+      if (lastName === undefined) {
+        alert(`Please enter a name in the box provided.`);
+        lastName = prompt('Employee Last Name');
+      };
+    console.log(`Last name entered: ${lastName}`);
+
+    let salary = prompt('Enter salary');
+      if (salary === null) {
+        console.log(`Operation cancelled at Salary entry`);
+        nextEmployee = false;
+        break;
+      };
+      //provide for if salary is blank
+      if (isNaN(salary)) { 
+        alert('Salary should be a numerical quanitity in US currency.');
+        salary = prompt('Enter salary');
+      };
+    console.log(`Salary entered: $${salary}`);
+
+    //collect employee info variables in an object
+    let employeeInfo = { 
+      firstName: firstName,
+      lastName: lastName,
+      salary: salary,
+    };
+    console.log(`Employee Info = ${employeeInfo}`);
+
+    //add completed object to array
+    employeesArray.push(employeeInfo); 
+    console.log(`Employee info pushed to array: ${employeeInfo}`);
+    
+    //prompt to add another employee
+    const goAgain = confirm(`Add another?`);
+      (!goAgain) ? (nextEmployee = !nextEmployee) : (nextEmployee = nextEmployee)
   
-    if (!nextEmployee) {return;};
+  //end of While loop
+  };
+  
+  //send array info to storage in memory
+  return employeesArray; 
 
-    if (isNaN(salary)) { //provide for if salary is blank or not a number
-      alert('We need to pay them something.')
-      salary = prompt('salary')
-    } 
-
-    let employeeInfo = { //collect employee info variables in an object
-    firstName: firstName,
-    lastName: lastName,
-    salary: salary,
-    }
-
-    employeesArray.push(employeeInfo); //add completed object to array
-    nextEmployee = confirm('Add another?'); //boolean to reinit while loop
-  }
-return employeesArray; //sends array info to storage in memory
-};  
-
-addEmployeesBtn.addEventListener('click', collectEmployees)
+//end Collect Employees function
+}; 
 
 // Display the average salary
 let totalSalary = 0
 const displayAverageSalary = function() {
   // TODO: Calculate and display the average salary
 for (let i=0; i<employeesArray.length; i++) {
-  (totalSalary += employeesArray[i].salary)
+  totalSalary = (totalSalary += employeesArray[i].salary)
 };
-totalSalary / employeesArray.length
+let averageSalary = (totalSalary / employeesArray.length);
+return console.log(`Average Salary: $${averageSalary}`);
 }; //end of display average salary code
 
 // Select a random employee
 const getRandomEmployee = function(employeesArray) {
   // TODO: Select and display a random employee
-  employeesArray[Math.floor(Math.random() * employeesArray.length)];
+  let randomEmployee = employeesArray[Math.floor(Math.random() * employeesArray.length)];
+  return console.log(`Congratulations, ${randomEmployee.firstName} ${randomEmployee.lastName}, you have been selected!`);
 }; //end of select random employee code
 
 /*
